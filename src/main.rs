9c79efdce54247a81540107ff2a061b58b330dc2
Vince <vincivenv@gmail.com>
2024-04-18 22:26:06 +0200

    pass 2nd stage

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::io::Write;
 // Uncomment this block to pass the first stage
 use std::net::TcpListener;
 
@@ -11,8 +12,9 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut _stream) => {
                 println!("accepted new connection");
+                _stream.write_all("HTTP/1.1 200 OK\r\n\r\n".as_ref()).unwrap()
             }
             Err(e) => {
                 println!("error: {}", e);
